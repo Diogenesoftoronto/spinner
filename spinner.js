b0665f49baf926e7b7bef4d06fdee7d7234204a9
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-30 20:13:25 -0500

    this is a spinner

---
+++ spinner.js
@@ -0,0 +1,20 @@
+process.stdout.write('hello from spinner.js... \rheyyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 800);
